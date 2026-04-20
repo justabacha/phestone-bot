2786c259c6d0a6d037d30b8383e3756ff00224e8
Phestone <just1abacha@gmail.com>
2026-04-20 13:57:39 +0300

    ``

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 module.exports = {
   // Command prefix (e.g., !help, /ping)
   prefix: '!',
-
   // Owner numbers (include country code, no '+')
   ownerNumbers: ['254768946798'],
-
   // Bot name (shown in help)
   botName: 'PHESTONE BOT',
-
   // Branding line (used in vault and other places)
   branding: '> *its-phestone*',
-
   // Auto-view status (implement in a separate module if needed)
   autoViewStatus: false,
   autoReactStatus: false,
